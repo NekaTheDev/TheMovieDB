0124fba2f600cac27b057aafbcbb8394f4d5b9ff
neka.the.developer@gmail.com <neka.the.developer@gmail.com>
2019-09-03 05:43:09 +0700

    Organized Project's Structure

---
+++ TheMovieDB/Supporting Files/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TheMovieDB
+//
+//  Created by NEKA on 9/3/19.
+//  Copyright © 2019 NEKA. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
